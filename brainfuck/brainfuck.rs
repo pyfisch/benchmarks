a12284391e97eb87a69dc99d22b3488b64b72894
Pyfisch <pyfisch@gmail.com>
2015-06-06 16:12:46 +0200

    Improve brainfuck.rs, use BTreeMap and other improvements.
    3 times faster: before 16.81s, 6.2Mb after 4.89s, 6.1Mb

--- brainfuck/brainfuck.rs
+++ brainfuck/brainfuck.rs
@@ -4,7 +4,7 @@ use std::io::prelude::*;
 use std::vec::Vec;
 use std::io;
 use std::env;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 struct Tape {
   pos: usize,
@@ -14,7 +14,7 @@ struct Tape {
 impl Tape {
   fn new() -> Tape { Tape { pos: 0, tape: vec![0] } }
   fn get(&self) -> isize { self.tape[self.pos] }
-  fn getc(&self) -> char { self.get() as u8 as char }
+  fn getc(&self) -> u8 { self.get() as u8 }
   fn inc(&mut self) { self.tape[self.pos] += 1; }
   fn dec(&mut self) { self.tape[self.pos] -= 1; }
   fn advance(&mut self) { self.pos += 1; if self.tape.len() <= self.pos { self.tape.push(0) } }
@@ -22,48 +22,46 @@ impl Tape {
 }
 
 struct Program {
-  code: Vec<char>,
-  bracket_map: HashMap<usize, usize>
+  code: Vec<u8>,
+  bracket_map: BTreeMap<usize, usize>
 }
 
 impl Program {
-  fn new(content: String) -> Program {
-    let mut code: Vec<char> = Vec::new();
-    let mut bracket_map = HashMap::new();
+  fn new(content: Vec<u8>) -> Program {
+    let mut code = Vec::new();
+    let mut bracket_map = BTreeMap::new();
     let mut leftstack = Vec::new();
-    let mut pc = 0;
 
-    for c in content.chars() {
-      match c {
-        '+' | '-' | '.' | ',' | '<' | '>' => (),
-        '[' => { leftstack.push(pc); },
-        ']' => match leftstack.pop() {
-          Some(left) => { bracket_map.insert(left, pc); bracket_map.insert(pc, left); }
-          None => ()
-        },
-        _ => { continue; }
+    for (pc, b) in content.iter().filter(|&&x| x == b'+' || x == b'-' || x == b'.' || x == b','
+            || x == b'<' || x == b'>' || x == b'[' || x == b']').map(|&x| x).enumerate() {
+      if b == b'[' {
+        leftstack.push(pc);
+      } else if b == b']' {
+        if let Some(left) = leftstack.pop() {
+          bracket_map.insert(left, pc);
+          bracket_map.insert(pc, left);
+        }
       }
-      code.push(c);
-      pc += 1;
+      code.push(b);
     }
     Program{ code: code, bracket_map: bracket_map }
   }
 
   fn run(&self) {
     let mut pc: usize = 0;
-    let len = self.code.len();
     let mut tape = Tape::new();
+    let mut stdout = io::stdout();
 
-    while pc < len {
+    while pc < self.code.len() {
       match self.code[pc] {
-        '+' => tape.inc(),
-        '-' => tape.dec(),
-        '>' => tape.advance(),
-        '<' => tape.devance(),
-        '[' => { if tape.get() == 0 { pc = self.bracket_map[&pc]; } },
-        ']' => { if tape.get() != 0 { pc = self.bracket_map[&pc]; } },
-        '.' => { print!("{}", tape.getc()); io::stdout().flush().unwrap() },
-        _ => ()
+        b'+' => tape.inc(),
+        b'-' => tape.dec(),
+        b'>' => tape.advance(),
+        b'<' => tape.devance(),
+        b'[' => { if tape.get() == 0 { pc = self.bracket_map[&pc]; } },
+        b']' => { if tape.get() != 0 { pc = self.bracket_map[&pc]; } },
+        b'.' => { stdout.write(&[tape.getc()]).unwrap(); stdout.flush().unwrap() },
+        _ => unreachable!()
       }
       pc += 1;
     }
@@ -71,10 +69,12 @@ impl Program {
 }
 
 fn main() {
-  let arg1 = env::args().nth(1).unwrap();
-  let path = Path::new(&arg1);
-  let mut s = String::new();
-  let mut file = File::open(&path).unwrap();
-  file.read_to_string(&mut s).unwrap();
-  Program::new(s).run()
+  let mut buf = Vec::new();
+  {
+    let arg1 = env::args().nth(1).unwrap();
+    let path = Path::new(&arg1);
+    let mut file = File::open(&path).unwrap();
+    file.read_to_end(&mut buf).unwrap();
+  }
+  Program::new(buf).run();
 }
